24ca050702668317f605d3b14c2110101eb5b5af
Yoshiki Kobayashi <kobayashi020202@gmail.com>
2024-09-03 01:05:51 +0900

    feat:　最低限アクセスログが出るように

--- separate-statis-files/src/app.ts
+++ separate-statis-files/src/app.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import logger from "morgan";
+import express, { Request, Response } from "express";
 import mysql, { ConnectionOptions } from 'mysql2/promise';
 
 dotenv.config();
@@ -17,22 +18,23 @@ const access: ConnectionOptions = {
 const conn = mysql.createConnection(access);
 
 app.set('view engine', 'ejs');
+app.use(logger("dev"));
 
 app.use('/', express.static(__dirname + '/../public'));
 
-app.get("/", async (request: Request, response: Response) => { 
+app.get("/", async (request: Request, response: Response) => {
   try {
-    const result = await((await conn).query('SELECT * FROM photos'));
+    const result = await ((await conn).query('SELECT * FROM photos'));
     response.render("./index.ejs", {
       items: result[0],
     });
   } catch (error: any) {
     throw new Error("Error: " + error.message);
   }
-}); 
+});
 
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
+app.listen(PORT, () => {
+  console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
   // エラーの処理
   throw new Error(error.message);

c2fe965990dac917f2ae6bf4f41ced3929e14f80
Yoshiki Kobayashi <kobayashi020202@gmail.com>
2024-09-04 17:15:45 +0900

    feat: 一旦別ディレクトリからソース類を移動

---
+++ tuning-mysql/migrate/create_table.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS photos;
+
+CREATE TABLE photos (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  url VARCHAR(255)
+);
+
+INSERT INTO
+  photos (id, name, url)
+VALUES
+  (
+    1,
+    'Photo 1',
+    '1.jpg'
+  ),
+  (
+    2,
+    'Photo 2',
+    '2.jpg'
+  ),
+  (
+    3,
+    'Photo 3',
+    '3.jpg'
+  ),
+  (
+    4,
+    'Photo 4',
+    '4.jpg'
+  ),
+  (
+    5,
+    'Photo 5',
+    '5.jpg'
+  ),
+  (
+    6,
+    'Photo 6',
+    '6.jpg'
+  );
